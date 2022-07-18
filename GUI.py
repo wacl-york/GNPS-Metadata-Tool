e0909eab780f7a5d5bc43370f1a5ba19b4bd17e3
Samuel Plane <90203630+SamuelPlane@users.noreply.github.com>
2022-07-18 18:02:39 +0100

    Grid layout (#28)
    * Changed UI to use grid when selecting files
    
    * Added functinality to add fields in grid form using both config files and manually
    
    * Toolbar (#21)
    
    * Updated docstrings
    
    * Submit button has been changed to work with the grid format
    
    * Made slight correction to WACLConfig
    
    * Started toolbar
    
    * Moved Start button into new toolbar, which also contains the new help button. WACL Logo has replaced the instructions in the bottom left
    
    * Improved code style
    
    * Added functionality to edit metadata files
    
    Co-authored-by: Samuel Plane <sam.plane@york.ac.uk>
    
    * Scrollbar (#27)
    
    * Beginning of implementation of scrollbars
    
    * Scrollbars now work
    
    * Scrollbars are now placed dynamically so that they fit to the cells in the table
    
    Co-authored-by: Samuel Plane <sam.plane@york.ac.uk>
    
    Co-authored-by: Samuel Plane <sam.plane@york.ac.uk>

--- GUI.py
+++ GUI.py
@@ -3,10 +3,13 @@ from tkinter import filedialog as fd
 from os import listdir
 from os.path import isfile, join
 import xml.etree.ElementTree as ET
+from PIL import ImageTk, Image
 import yaml
+import csv
 
 window = tk.Tk()
-window.geometry('1700x700')
+#window.configure(background='black')
+window.geometry('1200x700')
 window.title('Metadata Tool')
 
 window.columnconfigure(0, weight=1, minsize=200)
@@ -27,20 +30,66 @@ preview_frame = tk.Frame(
     master=window
     )
 
-scroll = tk.Scrollbar(master = preview_frame, orient='horizontal')
-scroll.pack(side=tk.BOTTOM, fill='x')
-preview = tk.Text(master = preview_frame, wrap=tk.NONE, xscrollcommand=scroll.set)
+toolbar_frame = tk.Frame(
+    master = window
+    )
+
+logo_frame = tk.Frame(
+    master=window,
+    width=10,
+    height=10
+    )
+
+grid_canvas = tk.Canvas(
+    master = preview_frame,
+    height = 200,
+    width = 700
+    )
+
+entry_frame = tk.Frame(
+    master = grid_canvas
+    )
+
+grid_canvas.create_window((0, 0), window=entry_frame, anchor='nw')
+
+vscroll = tk.Scrollbar(master = preview_frame, orient = 'vertical', command = grid_canvas.yview)
+hscroll = tk.Scrollbar(master = preview_frame, orient = 'horizontal', command = grid_canvas.xview)
+preview = tk.Text(master = preview_frame)
 fieldName = tk.Entry(master=side_frame)
 fieldDefault = tk.Entry(side_frame)
 fieldNameLabel = tk.Label(text='Field to add', master=side_frame)
 fieldDefaultLabel = tk.Label(text='Default value', master=side_frame)
-instructionsLabel = tk.Label(text='Instructions for use: \n 1. Put all the files you wish to create metadata \n for into one folder, then select this folder \n 2. The files within this folder will appear in the preview. \n From here you can add fields by either \n importing a config file containing all \n the fields. Alternatively you can add fields \n individually by entering the name of the field  \n and optionally a default value \n 3. From here you can modify the values as necessary \n 4. Click the "Save as .txt" button to finalise the folder')
-instructionsLabel.bind('<Configure>', lambda e: instructionsLabel.config(wraplength=instructionsLabel.winfo_width()))
 preview.insert('1.0', 'Start by selecting a folder containing the files to be uploaded')
-preview.pack(fill='both', expand=True)
-scroll.config(command=preview.xview)
-    
+preview.grid(row=0, column=0)
+#entry_frame.bind("<Configure>", grid_canvas.configure(scrollregion=grid_canvas.bbox("all")))
+
+#Opens logo in a PIL format so it can be resized
+logo = Image.open('wacl.png')
+logo_resized = logo.resize((350, 250))
+#Resized photo is used to create a new object tkinter will accept
+logo_photo_image = ImageTk.PhotoImage(logo_resized)
+logo_label = tk.Label(master = window, image = logo_photo_image)
+
 noOfFiles = 0
+grid = [[]]
+
+def adjustScrollRegion():
+    entry_frame.update_idletasks()
+    
+    if len(grid[0]) > 4:
+        #Constant assigned from experience
+        canvas_width = 700
+    else:
+        canvas_width = len((grid[0]) * grid[0][0].winfo_width())
+
+    if len(grid) > 9:
+        canvas_height = 200
+    else:
+        canvas_height = len(grid) * grid[0][0].winfo_height()
+    
+    grid_canvas.config(scrollregion=grid_canvas.bbox("all"), width = canvas_width, height = canvas_height)
+    
+    
 
 
 """
@@ -90,26 +139,21 @@ def getLineNo():
     
     return lines
 
+""" 
+Displays instructions for using the tool in a popup window
+
+Upon clicking the "Help" button, a new window is created, inside of which is a label containing the instructions,
+and a button, which will close the window when clicked.
 
 """
-Adds a new field to the preview
-
-Adds a field name to the top line and the default value to subsequent lines. All lines after the first are checked to find
-the end of the line, allowing the default value to be inserted in the correct place.
-"""
-def addField():
-    field = fieldName.get()
-    default = fieldDefault.get()
-    fieldName.delete(0, tk.END)
-    fieldDefault.delete(0, tk.END)
-    insertIndex = getEnd(1)
-    locationToInsert = '1.' + str(insertIndex)
-    preview.insert(locationToInsert, ', ATTRIBUTE_'+field)
-    #Adds the default value to the end of each line except the first
-    for each_line in range(preview.get('1.0', tk.END).count('\n') - 2):
-        lineNo = each_line + 2 #One to account for the line with the field names, one to account for the fact that lines are not zero-indexed
-        insertIndex = getEnd(lineNo)
-        preview.insert(str(lineNo) + '.' + str(insertIndex), ', ' + default)
+def showInstructions():
+    popupWindow = tk.Tk()
+    popupWindow.title('Instructions')
+    label = tk.Label(popupWindow, text = 'Instructions for use: \n 1. Put all the files you wish to create metadata \n for into one folder, then select this folder \n 2. The files within this folder will appear in the preview. \n From here you can add fields by either \n importing a config file containing all \n the fields. Alternatively you can add fields \n individually by entering the name of the field  \n and optionally a default value \n 3. From here you can modify the values as necessary \n 4. Click the "Save as .txt" button to finalise the folder')
+    label.pack()
+    closeButton = tk.Button(popupWindow, text = 'Close', command = popupWindow.destroy)
+    closeButton.pack()
+    popupWindow.mainloop()
 
 
 """
@@ -120,24 +164,83 @@ iteratively added to each line.
 """
 def addFiles():
     global noOfFiles
+    global grid
     noOfFiles = 0
     #Gets the files to add to the preview
     directoryToAdd = fd.askdirectory()
     if directoryToAdd != ():
+        grid = [[]]
+        preview.grid_forget()
         filesToAdd = [file for file in listdir(directoryToAdd) if isfile(join(directoryToAdd, file))]
-        #Adds the files to the preview under the filename column
-        preview.delete('1.0', tk.END)
-        preview.insert('1.0', 'filename\n')
-        lineNo=2
+        grid[0].append(tk.Entry(master = entry_frame))
+        grid[0][0].insert(0, 'filename')
+        grid[0][0].grid(row=0, column=0)
+        row = 1
+        #Creates an Entry object in grid, which corresponds to it's position in the the preview,
+        #which is then placed in the appropriate spot using the .grid method
         for eachFile in filesToAdd:
-            index = str(lineNo) + '.0'
-            preview.insert(index, eachFile + '\n')
-            lineNo += 1
-            noOfFiles += 1
+            grid.append([])
+            grid[row].append(tk.Entry(master = entry_frame))
+            grid[row][0].insert(0, eachFile)
+            grid[row][0].grid(row = row, column = 0)
+            row += 1
         
         fieldBtn['state']=tk.NORMAL
         submitBtn['state']=tk.NORMAL
         importBtn['state']=tk.NORMAL
+        adjustScrollRegion()
+
+
+def openFile():
+    global grid
+    filePath = fd.askopenfilename(filetypes=(("Metadata file", "*.txt"),))
+    if filePath != '':
+        preview.grid_forget()
+        grid = []
+        with open(filePath) as file:
+            tsv_file = csv.reader(file, delimiter="\t")
+            row_no = 0
+            for line in tsv_file:
+                grid.append([])
+                field_no = 0
+                for field in line:
+                    grid[row_no].append(tk.Entry(master = entry_frame))
+                    grid[row_no][field_no].insert(0, field)
+                    grid[row_no][field_no].grid(row=row_no, column = field_no)
+                    field_no += 1
+
+                row_no += 1
+
+        fieldBtn['state']=tk.NORMAL
+        submitBtn['state']=tk.NORMAL
+        importBtn['state']=tk.NORMAL
+        adjustScrollRegion()
+
+
+
+"""
+Adds a new field to the grid
+
+Appends a new entry containing the field name to the first line and the default value to subsequent lists.
+These entries are then added to the preview frame using the grid method in the position corresponding to
+it's location in the 2d list
+"""
+def addField():
+    field = fieldName.get()
+    default = fieldDefault.get()
+    fieldName.delete(0, tk.END)
+    fieldDefault.delete(0, tk.END)
+    grid[0].append(tk.Entry(master = entry_frame))
+    grid[0][-1].insert(0, 'ATTRIBUTE_'+field)
+    grid[0][-1].grid(row=0, column = len(grid[0])-1)
+    row_no = 1
+    for each_line in range(len(grid)-1):
+        grid[row_no].append(tk.Entry(master = entry_frame))
+        grid[row_no][-1].insert(0, default)
+        grid[row_no][-1].grid(row=row_no, column = len(grid[row_no]) - 1)
+        row_no += 1
+
+    adjustScrollRegion()
 
 
 def submit():
@@ -148,19 +251,37 @@ def submit():
         if path.find('.') != -1:
             path = path[0:path.find('.')]
             path += '.txt'
-            
+
+            writeFile = open(path, 'w')
+            for each_line in grid:
+                new_line = True
+                for each_field in each_line:
+                    if new_line == True:
+                        new_line = False
+                        writeFile.write(each_field.get())
+                    else:
+                        writeFile.write('\t')
+                        writeFile.write(each_field.get())
+
+                writeFile.write('\n')
+                        
+                
+                    
+
+        """
         fileContents = preview.get('1.0', tk.END)
         fileContents = fileContents.replace(',', '\t')
         writeFile = open(path, 'w')
         writeFile.write(fileContents)
         writeFile.close()
+        """
 
 """
-Reads in an xml file and adds the fields it contains to the preview
+Reads in a yaml file and adds the fields it contains to the preview
 
 User selects a file, which is then parsed to get two lists containing the names
 and default values of each field. These two lists are then iterated through to
-add all the fields to the preview in the same manner as addFiles()
+add all the fields to the grid in the same manner as addFiles()
 """
 def importConfig():
     #User selects config file
@@ -187,22 +308,42 @@ def importConfig():
         for each_field in range(len(names)):
             field = names[each_field]
             default = values[each_field]
-            insertIndex = getEnd(1)
-            locationToInsert = '1.' + str(insertIndex)
-            preview.insert(locationToInsert, ', ATTRIBUTE_' + field)
-            #Adds the default value to the end of each line except the first
+            grid[0].append(tk.Entry(master = grid_canvas))
+            grid[0][-1].insert(0, 'ATTRIBUTE_'+field)
+            grid[0][-1].grid(row=0, column = len(grid[0])-1)
+            row_no = 1
+            for each_line in range(len(grid)-1):
+                grid[row_no].append(tk.Entry(master = grid_canvas))
+                grid[row_no][-1].insert(0, default)
+                grid[row_no][-1].grid(row=row_no, column = len(grid[row_no]) - 1)
+                row_no += 1
 
-            #There needs to be two less lines due to the lack of zero-indexing and the first line being irrelevant here
-            for each_line in range(preview.get('1.0', tk.END).count('\n') - 2):
-                lineNo = each_line + 2 #Similiarly, the working line is increased by two to ensure the first line is skipped
-                insertIndex = getEnd(lineNo)
-                preview.insert(str(lineNo) + '.' + str(insertIndex), ', ' + default)
+        adjustScrollRegion()
+                
+        """
+        insertIndex = getEnd(1)
+        locationToInsert = '1.' + str(insertIndex)
+        preview.insert(locationToInsert, ', ATTRIBUTE_' + field)
+        #Adds the default value to the end of each line except the first
+
+        #There needs to be two less lines due to the lack of zero-indexing and the first line being irrelevant here
+        for each_line in range(preview.get('1.0', tk.END).count('\n') - 2):
+            lineNo = each_line + 2 #Similiarly, the working line is increased by two to ensure the first line is skipped
+            insertIndex = getEnd(lineNo)
+            preview.insert(str(lineNo) + '.' + str(insertIndex), ', ' + default)
+        """
 
 
 filesBtn = tk.Button(
-    text = 'Select Folder',
+    text = 'Create new file',
     command=addFiles,
-    master = lower_frame
+    master = toolbar_frame
+    )
+
+openBtn = tk.Button(
+    text = 'Open file',
+    command=openFile,
+    master = toolbar_frame
     )
 
 fieldBtn = tk.Button(
@@ -226,12 +367,27 @@ importBtn = tk.Button(
     master = lower_frame
     )
 
-preview_frame.grid(row=0, column=0, sticky='NESW')
-lower_frame.grid(row=1, column=0)
-side_frame.grid(row=0, column=1)
+instructionsBtn = tk.Button(
+    text = 'Help',
+    master = toolbar_frame,
+    command = showInstructions
+    )
+
+toolbar_frame.grid(row=0, column=0, sticky = 'NESW')
+preview_frame.grid(row=1, column=0, sticky='NESW')
+grid_canvas.grid(row=0, column=0)
+grid_canvas.grid_rowconfigure(0, weight=1)
+grid_canvas.grid_columnconfigure(0, weight=1)
+grid_canvas.configure(yscrollcommand = vscroll.set)
+grid_canvas.configure(xscrollcommand = hscroll.set)
+preview_frame.grid_propagate(False)
+vscroll.grid(row=0, column=1, sticky = 'NS')
+hscroll.grid(row=1, column=0, sticky = 'EW')
+lower_frame.grid(row=2, column=0)
+side_frame.grid(row=1, column=1)
+logo_label.grid(row=2, column=1, sticky='NESW')
 
 #lower frame
-filesBtn.grid(row=0, column=0)
 importBtn.grid(row=1, column=0)
 submitBtn.grid(row=2, column=0)
 
@@ -242,6 +398,11 @@ fieldDefaultLabel.grid(row=2, column=0)
 fieldDefault.grid(row=3, column=0)
 fieldBtn.grid(row=4, column=0)
 
-instructionsLabel.grid(row=1, column=1)
+#toolbar frame
+filesBtn.grid(row=0, column=0)
+openBtn.grid(row=0, column=1)
+instructionsBtn.grid(row=0, column=2)
+
+#imageLabel.pack()
 
 window.mainloop()
--- README.md
+++ README.md
@@ -6,3 +6,9 @@ When working on linux you will need to install tkinter by running
 <i>sudo apt-get install python3-tk</i>
 
 in the terminal. Tkinter comes as default with the windows distribution of Python so no extra work is needed to develop on that platform
+
+You will also need to install Pillow for displaying the WACL logo, which can be done by running
+
+<i>python3 -m pip install --upgrade Pillow</i>
+
+This needs to be done for either distribution of Python
