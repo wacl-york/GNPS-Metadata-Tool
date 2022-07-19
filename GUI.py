93fb7e464bd09c822e55c457d1ef5022db169dd8
Samuel Plane <sam.plane@york.ac.uk>
2022-07-19 16:03:09 +0100

    Added a link to the tool's github repository issues page to report bugs

--- GUI.py
+++ GUI.py
@@ -148,9 +148,20 @@ and a button, which will close the window when clicked.
 """
 def showInstructions():
     popupWindow = tk.Tk()
+    popupWindow.geometry('450x300')
     popupWindow.title('Instructions')
-    label = tk.Label(popupWindow, text = 'Instructions for use: \n 1. Put all the files you wish to create metadata \n for into one folder, then select this folder \n 2. The files within this folder will appear in the preview. \n From here you can add fields by either \n importing a config file containing all \n the fields. Alternatively you can add fields \n individually by entering the name of the field  \n and optionally a default value \n 3. From here you can modify the values as necessary \n 4. Click the "Save as .txt" button to finalise the folder')
+    label = tk.Label(popupWindow, text = 'Instructions for use: \n 1. Put all the files you wish to create metadata \n for into one folder, then select this folder \n 2. The files within this folder will appear in the preview. \n From here you can add fields by either \n importing a config file containing all \n the fields. Alternatively you can add fields \n individually by entering the name of the field  \n and optionally a default value \n 3. From here you can modify the values as necessary \n 4. Click the "Save as .txt" button to finalise the folder. \n If you encounter any issues you can report them at \n')
     label.pack()
+    link = tk.Text(popupWindow, height = 1)
+    link.insert(1.0, 'https://github.com/wacl-york/GNPS-Metadata-Tool/issues')
+    link.pack()
+
+    def focusText(event):
+        link.config(state='normal')
+        link.focus()
+        link.config(state='disabled')
+    
+    link.bind('<Button-1>', focusText)
     closeButton = tk.Button(popupWindow, text = 'Close', command = popupWindow.destroy)
     closeButton.pack()
     popupWindow.mainloop()
